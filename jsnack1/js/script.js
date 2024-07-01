163b58af3b3154661a9f4509f4077b3eb1b1eefb
andreacagnina <amirkal@libero.it>
2024-07-01 15:54:35 +0200

    [FEAT] COMPLETED SNACK 1. DEL FOR CICLY, ADD INCLUDES PROPR

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,4 +1,34 @@
 let btn = document.getElementById('btn');
+let vip = ['andrea', 'mario', 'girolamo', 'giacinto', 'filippo', 'osvaldino'];
+console.log(vip);
 
-let btn
+btn.addEventListener('click', function () {
+
+    let name = document.getElementById('name').value.toLowerCase();
+
+    console.log(name);
+
+    // *MOD invece che utilizzare il ciclo FOR ho applicato includes, argomento utilizzato oggi a lezione.
+
+    // for (let i = 0; i < vip.length; i++) {
+    //     if (vip[i] == name) {
+    //         console.log('Puoi partecipare alla festa');
+    //     }
+
+    //     else {
+    //         console.log('Non puoi partecipare alla festa');
+    //     }
+    // }
+
+    let checkName = vip.includes(name);
+
+    if (checkName) {
+        console.log('Puoi partecipare alla festa');
+    }
+
+    else {
+        console.log('Non puoi partecipare alla festa');
+    }
+
+})
 
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ SNACK1
 -attraverso click su button, estraggo il valore dell'input inserito
 -creo un array con i nomi degli invitati che possono accedere
 -attraverso un ciclo for con if controllo se il valore dell'input è presente all'interno di un array 
+
+*MOD invece che utilizzare il ciclo FOR ho applicato includes, argomento utilizzato oggi a lezione.
