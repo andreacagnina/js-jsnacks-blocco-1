cbd94090d6dcba92f51c48886bbac3d5dc5441df
andreacagnina <amirkal@libero.it>
2024-07-01 18:37:47 +0200

    [FEAT] completed snack 3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -2,7 +2,22 @@ let btn = document.getElementById('btn');
 
 
 btn.addEventListener('click', function () {
+    let somma = 0;
 
+    let number = document.getElementById('number').value;
+    console.log(number)
+
+    MyElement = number.split("");
+    console.log(MyElement)
+
+    for (i = 0; i < MyElement.length; i++) {
+        // console.log(MyElement[i])
+        somma += parseInt(MyElement[i]);
+        console.log(somma)
+
+        let result = document.getElementById('result');
+        result.innerHTML = somma;
+    }
 
 
 })
