aafee821b0738e13c97b226e30fc4653317b3adf
andreacagnina <amirkal@libero.it>
2024-07-01 18:51:21 +0200

    [FEAT] ADDED CONTROL CONDITION: min & max length on input

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -2,21 +2,30 @@ let btn = document.getElementById('btn');
 
 
 btn.addEventListener('click', function () {
-    let somma = 0;
-
     let number = document.getElementById('number').value;
     console.log(number)
 
-    MyElement = number.split("");
-    console.log(MyElement)
+    if (number.length == 4) {
 
-    for (i = 0; i < MyElement.length; i++) {
-        // console.log(MyElement[i])
-        somma += parseInt(MyElement[i]);
-        console.log(somma)
+        let somma = 0;
 
+        MyElement = number.split("");
+        console.log(MyElement)
+
+        for (i = 0; i < MyElement.length; i++) {
+            console.log(MyElement[i])
+            somma += parseInt(MyElement[i]);
+            console.log(somma)
+
+            let result = document.getElementById('result');
+            result.innerHTML = somma;
+        }
+
+    }
+
+    else {
         let result = document.getElementById('result');
-        result.innerHTML = somma;
+        result.innerHTML = "Devi inserire un numero a 4 cifre!";
     }
 
 
